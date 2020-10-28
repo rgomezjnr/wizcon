8c148cfe1026b4027fd38f7f7a2b223686dde6f7
rgomezjnr <rgomezjnr@gmail.com>
2020-10-28 13:38:13 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wizctrl",
+    version="0.1.0",
+    author="Robert Gomez, Jr.",
+    author_email="rgomezjnr@gmail.com",
+    description="Control Philips WiZ smart light bulbs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rgomezjnr/wizctrl",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=["pywizlight"],
+    license="MIT",
+    keywords="philips wiz wifi wireless home automation cli tool",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Home Automation",
+        "Topic :: Utilities",
+    ],
+)
